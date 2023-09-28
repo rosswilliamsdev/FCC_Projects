fae94d1ddac289f375b59870e2ad57fe38df16a9
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-09-28 12:30:19 -0500

    remove excess .toLowerCase()

--- palindrome_checker.js
+++ palindrome_checker.js
@@ -2,7 +2,7 @@ function palindrome(str) {
   let regex = /[^A-Za-z0-9]/g;
   str = str.toLowerCase();
   str = str.replace(regex, "");
-  let strArray = Array.from(str.trim().toLowerCase());
+  let strArray = Array.from(str.trim());
   let reverseStr = strArray.reverse().join("");
 
   if (str === reverseStr) {
