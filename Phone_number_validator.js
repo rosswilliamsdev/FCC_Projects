5fd17145d85a9ffe9ab363ba6e7524bcae0422e7
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-10-03 14:06:44 -0500

    early return fix

--- Phone_number_validator.js
+++ Phone_number_validator.js
@@ -2,19 +2,26 @@ function telephoneCheck(str) {
   const regex = /([0-9\-\(\)\s])/g;
   let match;
 
-  if (str.length >= 8 && str.length <= 14) {
+  if (str.length < 8 || str.length > 14) {
+    console.log("false: length");
     return false;
   }
 
   for (let i = 0; i < str.length; i++) {
     match = str[i].match(regex);
-    console.log(match);
     if (match === null) {
+      console.log("false: invalid characters");
       return false;
     }
   }
-
+  console.log("true");
   return true;
 }
 
-telephoneCheck("555-555-5555");
+telephoneCheck("1 555 555 5555");
+
+// country number must be 1 if included
+// area code 3 digits plus possibly paranthesis
+// phone number 7 digits plus possibly hyphen
+// spaces between country, area code, and phone possible
+// hyphens between area code & phone number
