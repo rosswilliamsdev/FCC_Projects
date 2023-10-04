c30ecd54fb58cd1c376ea039c1aa198bfcdbe36d
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-10-04 14:56:40 -0500

    solved for country code

--- Phone_number_validator.js
+++ Phone_number_validator.js
@@ -1,10 +1,18 @@
 function telephoneCheck(str) {
   const regex = /([0-9\-\(\)\s])/g;
+  const lookAround = /(0-9)([?=\/\(\)]) /g;
   let match;
 
-  if (str.length < 8 || str.length > 14) {
+  if (str.length < 8 || str.length > 16) {
     console.log("false: length");
     return false;
+  }
+
+  if (str[0] === "1") {
+    if (str[1] !== " " && str[1] !== "(") {
+      console.log("false: str[1] invalid");
+      return false;
+    }
   }
 
   for (let i = 0; i < str.length; i++) {
@@ -14,14 +22,20 @@ function telephoneCheck(str) {
       return false;
     }
   }
+
   console.log("true");
   return true;
 }
 
-telephoneCheck("1 555 555 5555");
+telephoneCheck("1 (555)555-5555");
 
 // country number must be 1 if included
 // area code 3 digits plus possibly paranthesis
 // phone number 7 digits plus possibly hyphen
 // spaces between country, area code, and phone possible
 // hyphens between area code & phone number
+
+// use regex lookahead/lookbehind
+
+// eliminate phone numbers based on characters ahead of them
+// positive or negative lookahead?
