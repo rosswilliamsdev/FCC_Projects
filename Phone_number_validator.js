e5c3d220907af0056086654a4ad6b510e8562d8e
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-10-05 13:46:03 -0500

    old code comments

--- Phone_number_validator.js
+++ Phone_number_validator.js
@@ -39,3 +39,44 @@ telephoneCheck("1 (555)555-5555");
 
 // eliminate phone numbers based on characters ahead of them
 // positive or negative lookahead?
+
+
+
+
+// for (let i = 0; i < str.length; i++) {
+//   if (digits.test(str[i]) === true) {
+//     digitsArray.push(Number(str[i]));
+//   }
+// }
+
+// if (firstDigit.test(str) === false) {
+//   console.log("false: first char");
+//   return false;
+// }
+
+// if (digitsArray.length !== 10 && digitsArray.length !== 11) {
+//   console.log("false: length");
+//   return false;
+// }
+
+// if (digitsArray.length === 11) {
+//   if (digitsArray[0] !== 1) {
+//     console.log("false: wrong country code");
+//     return false;
+//   }
+// }
+
+// if (str[0] === "1") {
+//   if (str[1] !== " " && str[1] !== "(") {
+//     console.log("false: str[1] invalid");
+//     return false;
+//   }
+// }
+
+// for (let i = 0; i < str.length; i++) {
+//   match = str[i].match(regex);
+//   if (match === null) {
+//     console.log("false: invalid characters");
+//     return false;
+//   }
+}
