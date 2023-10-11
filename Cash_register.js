e49545624bcd88693c8d6cdeacef312c3b580ddb
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-10-10 23:43:33 -0500

    brainstorming

--- Cash_register.js
+++ Cash_register.js
@@ -11,13 +11,34 @@ function checkCashRegister(price, cash, cid) {
     totalCID += cid[i][1];
   }
 
+  let currency = {
+    penny: cid[0][1] / 0.01,
+    nickel: Math.round(cid[1][1] / 0.05),
+    dime: cid[2][1] / 0.1,
+    quarter: cid[3][1] / 0.25,
+    one: cid[4][1] / 1,
+    five: cid[5][1] / 5,
+    ten: cid[6][1] / 10,
+    twenty: cid[7][1] / 20,
+    hundred: cid[8][1] / 100,
+  };
+
   if (changeDue > totalCID) {
     changeObject.status = "INSUFFICIENT_FUNDS";
     console.log(changeObject);
     return changeObject;
   }
 
-  console.log("Last return:", changeObject);
+  for (let i = 0; i < cid.length; i++) {}
+
+  // This fits somehow I know it
+  for (let i = 0; i < cid.length; i++) {
+    while (changeDue >= cid[i][1]) {
+      changeDue -= cid[i][1];
+    }
+  }
+
+  // console.log('Last return:', changeObject)
   return changeObject;
 }
 
